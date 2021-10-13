203fa4c9467cf70b8cacec0adb620a82c62bb6df
josefigarola <jmfigarolaprado2@gmail.com>
2021-10-13 17:16:16 -0500

    Rand updated

--- labs/04/pi.c
+++ labs/04/pi.c
@@ -11,7 +11,7 @@ Threads */
 
 #define POINTS 1000000
 // Threads to be used
-#define nThreads 4 
+#define nThreads 2
 
 // Work to be done per thread
  int work_thread = POINTS/nThreads;
@@ -22,14 +22,14 @@ int count = 0; /* # of points in the 1st quadrant of unit circle */
 double z; // distance between points
 double pi;
 
-// pthread_t thread[nThreads];
-
-unsigned int SEED;
+unsigned int SEED; // rand and time
 
 // Function to generate a random number
 double GenerateRAND(){
     srand(time(NULL));
-    return (double) rand()/RAND_MAX;
+    double rnum = (double)rand_r(&SEED) / (double)((unsigned)RAND_MAX + 1);
+    return rnum;
+
 }
 
 /* Function to compute the value of pi
@@ -66,7 +66,9 @@ int main(){
         pthread_join(thread[i], NULL);
     }
 
-    pi = (double)count/POINTS*4;
+    // Calculate pi
+    pi = ((double)count/POINTS*4)/nThreads;
+
 
     // Time the process
     clock_t end = clock();
