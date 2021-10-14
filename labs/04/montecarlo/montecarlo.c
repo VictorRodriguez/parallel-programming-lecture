9ff81604acafdc035e2d5ca06dd956c428439aaf
OSWA00 <oswahdez00@gmail.com>
2021-10-14 13:26:48 -0500

    fix random function

--- labs/04/montecarlo/montecarlo.c
+++ labs/04/montecarlo/montecarlo.c
@@ -11,18 +11,22 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <time.h>
+#include <limits.h>
 #include <semaphore.h>
+#include <time.h>
 
-#define n_threads 4     // Num of threads
-#define n_points 100000  // Num of iterations
+#define n_threads 16            /* Num of threads */
+#define n_points 100000         /* Num of iterations */
 
-pthread_t tid[n_threads];
-sem_t mutex;
-
-int count = 0;
-double pi;
 int iter_per_thread = n_points / n_threads;
+
+pthread_t tid[n_threads];       /* Threads IDs */
+sem_t mutex;                    /* Sync bit */
+
+int count = 0;                  /* Total points inside the circle */
+double pi = 0;                  /* Variable for calculated pi vavlue */
+
+unsigned int seed = 676767676 ; /* Seed for random number */
 
 
 /*
@@ -30,14 +34,16 @@ int iter_per_thread = n_points / n_threads;
  * --------------------
  * Generates a random decimal number between 0 and 1
  * 
- * Taken from https://stackoverflow.com/a/3067387
+ * Random number generator with linear congruential generator
+ * 
+ * Taken from “Numerical Recipes” by William H. Press, Chapter 7: Random Numbers
  *
  *  returns: random double
  */
 double generateRandom()
 {
-    srand( time(NULL) );
-    return (double) rand() / RAND_MAX; 
+    seed = seed * 1103515245 + 123456;
+    return seed / (double)UINT_MAX; 
 }
 
 /*
@@ -51,7 +57,6 @@ void *calculatePi(void *arg)
 {    
     for (int i = 0; i < iter_per_thread; i++)
     {
-        sem_wait(&mutex);
         double x = generateRandom();
         double y = generateRandom();
 
@@ -59,8 +64,9 @@ void *calculatePi(void *arg)
 
         if (result <= 1)
         {
-            sem_post(&mutex);
+            sem_wait(&mutex);
             count++;
+            sem_post(&mutex);
         }
     }
     return NULL;
@@ -68,7 +74,7 @@ void *calculatePi(void *arg)
 
 int main()
 {
-
+    clock_t begin = clock();
     sem_init(&mutex, 0, 1); /* Initialize semaphore */
 
     /* Create threads */
@@ -83,9 +89,11 @@ int main()
         pthread_join(tid[i], NULL);
     }
 
-    pi= (double) count / n_points * 4;
+    pi = ( (double)count / (double) n_points) * 4.0;
 
-    printf("# of trials = %d , estimate of pi is %1.16f AND an absolute error of %g\n", n_points, pi, fabs(pi - M_PI));
-            
+    printf("# of trials = %d, estimate of pi is %1.16f and an absolute error of %g\n", n_points, pi, fabs(pi - M_PI));
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %4f seconds || Threads: %d threads\n", time_spent, n_threads);
     return 0;
 }
