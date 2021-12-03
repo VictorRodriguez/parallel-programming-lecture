5fe88ad19d9ccc348c9d33e2a50a152d3e78430c
Giancarlo <73549196+Ginfinite@users.noreply.github.com>
2021-12-03 14:21:50 -0600

    Homework#3_A01638108

---
+++ labs/07/OpenMp_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void Hello(void); //thread function
+
+int main(int argc, char* argv[]){
+	//get number of threads from command line
+	int thread_count = strtol(argv[1], NULL, 10);
+
+	# pragma omp parallel num_threads(thread_count)
+	Hello();
+
+	return 0;
+}
+
+void Hello(void){
+	int my_rank = omp_get_thread_num();
+	int thread_count = omp_get_num_threads();
+
+	printf("Hello from thread %d of %d\n", my_rank, thread_count);
+}
